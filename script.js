27977731b8bcea221619aba38179e708b66919da
Vishnu <vishnujith098@gmail.com>
2023-11-13 23:13:57 +0530

    Initian Commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // script.js
-const correctAnswer = "correct"; // Replace with the actual answer
+const correctAnswer = "post office"; // Replace with the actual answer
 
 function checkAnswer() {
     const userAnswer = document.getElementById("answer").value.toLowerCase();
 
     if (userAnswer === correctAnswer) {
-        document.getElementById("correct").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: jfjfkf</h1>';
+        document.getElementById("post office").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: jfjfkf</h1>';
         
     } else {
-        document.getElementById("correct").innerHTML = '<h1>Oops.Try again</h1>';
+        document.getElementById("post office").innerHTML = '<h1>Oops.Try again</h1>';
     }
 }
