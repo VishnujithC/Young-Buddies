3dda3ea02e7d739b58440e2c63baf374d107d218
Vishnu <vishnujith098@gmail.com>
2023-11-13 23:22:29 +0530

    Updated. Finalized .

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function checkAnswer() {
     const userAnswer = document.getElementById("answer").value.toLowerCase();
 
     if (userAnswer === correctAnswer) {
-        document.getElementById("post office").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: jfjfkf</h1>';
+        document.getElementById("correct").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: jfjfkf</h1>';
         
     } else {
-        document.getElementById("post office").innerHTML = '<h1>Oops.Try again</h1>';
+        document.getElementById("correct").innerHTML = '<h1>Oops.Try again</h1>';
     }
 }
