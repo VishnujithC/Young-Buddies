e300a3c12bfd257fb60580cd3c706adeaac1c849
Vishnu <vishnujith098@gmail.com>
2023-11-14 21:34:31 +0530

    Clue_change

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function checkAnswer() {
     const userAnswer = document.getElementById("answer").value.toLowerCase();
 
     if (userAnswer === correctAnswer) {
-        document.getElementById("correct").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: jfjfkf</h1>';
+        document.getElementById("correct").innerHTML = '<h1>Congratulations! You solved the riddle. Clue: WATER FILTER>';
         
     } else {
         document.getElementById("correct").innerHTML = '<h1>Oops.Try again</h1>';
